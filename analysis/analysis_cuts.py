97d3855e7d36b98871018fbf8f0f238e7afd00ed
Zachary Hulcher <12894838+zhulcher@users.noreply.github.com>
2024-07-30 14:04:38 -0700

    comments and alg updates

--- analysis/analysis_cuts.py
+++ analysis/analysis_cuts.py
@@ -1,39 +1,47 @@
+# from pandas import Float32Dtype
+import spine
 from spine.utils.globals import *
 import numpy as np
 from scipy import stats
 
+
 #TODO things I would like added in truth:
     #children_id
 
+#TODO label functions with keyword from slides
+# import moduleboundaries
 
-import moduleboundaries
+moduleboundaries=np.array([[-63.931,  63.931],
+                           [-62.076,  62.076],
+                           [-64.538,  64.538]])
 
 HIP=2
 MIP=3
 
-def is_contained(pos,eps=30):
+FinalParticle=spine.RecoParticle|spine.TruthParticle
+
+def is_contained(pos:np.ndarray,eps=30)->bool:
     '''
     Checks if a position is within the defined module boundaries within some tolerance, eps
-
+    #TODO specify type with colon and units 
     Parameters
     ----------
     pos : np.ndarray
-        (3) Vector position
+        (3) Vector position (cm)
     M : float/np.ndarray
-        Tolerance from module boundaries
+        Tolerance from module boundaries (cm)
 
     Returns
     -------
     Bool
         Point at least eps inside of boundaries 
         '''
-    for i in range(3):
-        if pos[i]<moduleboundaries[i][0]+eps: return False
-        if pos[i]>moduleboundaries[i][1]-eps: return False
+    if np.any(pos<moduleboundaries.T[0]+eps): return False
+    if np.any(pos>moduleboundaries.T[1]-eps): return False
     return True
 
 
-def HIPMIP_pred(particle,sparse3d_pcluster_semantics_HM,cluster3d_pcluster):
+def HIPMIP_pred(particle:FinalParticle,sparse3d_pcluster_semantics_HM):
     '''
     Returns the semantic segmentation prediction encoded in sparse3d_pcluster_semantics_HM,
     where the prediction is not guaranteed unique for each cluster, for the particle object,
@@ -45,71 +53,100 @@ def HIPMIP_pred(particle,sparse3d_pcluster_semantics_HM,cluster3d_pcluster):
         Particle object with cluster information and unique semantic segmentation prediction
     sparse3d_pcluster_semantics_HM : ????????
         New semantic segmentation predictions for each voxel in an image
-    cluster3d_pcluster: ????????
-        Map from particle object to voxels associated with that particle object
+
+    #TODO use index from spine.Particle 
+    #TODO do quality check 
 
     Returns
     -------
     int
         Semantic segmentation prediction including HIP/MIP for a cluster
     '''
-
+    
     #TODO I don't believe for a second I have the logic for this one right yet.
     #TODO I think I want to rework this to do the semantic segmentation all at once,
         # and store the value in a dictionary or something I can pass between functions that need it. 
 
     if len(particle.depositions)==0:return None
     shapelist=[]
-    voxelmap = cluster3d_pcluster
+    mine=particle.index
+
     # truthinfo = particle_pcluster
-    semantics_HM = sparse3d_pcluster_semantics_HM
-    semanticsdict={}
-    for i in range(len(semantics_HM.as_vector())): semanticsdict[semantics_HM.as_vector()[i].id()]=i
-    for k in range(len(voxelmap.as_vector()[particle.id])):
-        id=voxelmap.as_vector()[particle.id].as_vector()[k].id()
-        # print(semanticsdict[id],id, semantics_HM.as_vector()[semanticsdict[id]].value())
-        shapelist+=semantics_HM.as_vector()[semanticsdict[id]].value()
+    # semantics_HM = sparse3d_pcluster_semantics_HM
+    # semanticsdict={}
+    # for i in range(len(semantics_HM.as_vector())): semanticsdict[semantics_HM.as_vector()[i].id()]=i
+    # for k in range(len(voxelmap.as_vector()[particle.id])):
+    #     id=voxelmap.as_vector()[particle.id].as_vector()[k].id()
+    #     # print(semanticsdict[id],id, semantics_HM.as_vector()[semanticsdict[id]].value())
+    #     shapelist+=semantics_HM.as_vector()[semanticsdict[id]].value()
 
     return stats.mode(shapelist)
-
-def HIP_range(particle): 
-    '''
-    Returns the track length for a contained particle object identified as a HIP through
-    semantic segmentation prediction, with particles not satisfying these assumptions
-    assigned zero range.
-
-    Parameters
-    ----------
-    particle : spine.Particle
-        Particle object with cluster information
-
-    Returns
-    -------
-    float
-        Range of particle
-    '''
-    if HIPMIP_pred(particle,etc)!=HIP: return 0
-    if not particle.is_contained: return 0
-    return np.linalg.norm(particle.position-particle.end_position)
     
-def forwardness(particle):
+def direction_acos(particle:FinalParticle, beam_dir=np.array([0.,0.,1.])) -> float:
     '''
-    Returns 'forwardness' defined as the angle between the beam-axis (here assumed in z) and the
-    particle object's momentum
+    Returns angle between the beam-axis (here assumed in z) and the particle object's momentum
 
     Parameters
     ----------
     particle : spine.Particle
         Particle object with cluster information
+    beam_dir : np.ndarray[float]
     
     Returns
     -------
     float
         Angle between particle direction and beam
     '''
-    return np.arccos(particle.momentum[2]/np.linalg.norm(particle.momentum))
+    return np.arccos(np.dot(particle.start_dir,beam_dir))
+
+def collision_distance(particle1:FinalParticle,particle2:FinalParticle):
+    '''
+    Returns for each particle, the distance from the start point to the point along the vector start direction
+    which is the point of closest approach to the other particle's corresponding line, along with the distance of closest approach.
+    The parameters, t1 and t2, are calculated by minimizing ||p1+v1*t1-p2-v2*t2||^2, where p1/p2 are the starting point of each particle
+    and v1/v2 are the start direction of each particle
+
     
-def dist_hipend_mipstart(particle,hip_candidates):
+    Parameters
+    ----------
+    particle1 : spine.Particle
+        Particle object with cluster information
+    particle2 : spine.Particle
+        Particle object with cluster information
+    
+    Returns
+    -------
+    [float,float,float]
+        [t1,t2, min_{t1,t2}(||p1+v1*t1-p2-v2*t2||^2)]
+    '''
+    v1=particle1.start_dir
+    v2=particle2.start_dir
+
+    p1=particle1.start_point
+    p2=particle2.start_point
+
+    v11=np.dot(v1,v1)
+    v22=np.dot(v2,v2)
+    v12=np.dot(v1,v2)
+    dp=p1-p2
+
+    denom=v12**2==v11*v22
+
+    if denom==0: return [0,0,np.linalg.norm(dp)]
+
+    t1=(np.dot(v1,dp)*v22-v12*np.dot(v2,dp))/denom
+    t2=(v12*np.dot(v1,dp)-np.dot(v2,dp)*v11)/denom
+
+    min_dist=np.dot(p1+v1*t1-p2-v2*t2,p1+v1*t1-p2-v2*t2)
+
+    return [t1,t2,min_dist]
+
+#TODO angular resolution check
+#TODO direction estimate check 
+
+    
+def dist_hipend_mipstart(particle:FinalParticle,hip_candidates:list[list[FinalParticle]]):
+    
     '''
     Returns minimum distance between the start of a mip candidate and the end
     of every hip candidate supplied, along with the hip candidate identified.
@@ -121,40 +158,22 @@ def dist_hipend_mipstart(particle,hip_candidates):
     particle : spine.Particle
         Particle object with cluster information
     hip_candidates: List(List(spine.Particle))
-        List of spine particle objects corresponding to identified kaons, with the associated event number
+        List of spine particle objects corresponding to identified kaons
     
     Returns
     -------
-    (float,[int, spine.Particle])
-        Distance from hip to mip candidate and a list of event number and identified kaon candidate 
+    (float,[spine.Particle,spine.Particle])
+        Distance from hip to mip candidate and identified kaon candidate 
 
     '''
-    if HIPMIP_pred(particle,etc)!=MIP: return (np.inf, None)
     shortest_dist=(np.inf, None)
     for h in hip_candidates:
-        if np.linalg.norm(h.end_position-particle.position)<shortest_dist:
-            shortest_dist=np.linalg.norm(hip_candidates.end_position-particle.position)
+        if np.linalg.norm(h[0].end_point-particle.start_point)<shortest_dist:
+            shortest_dist=np.linalg.norm(h[0].end_point-particle.start_point)
             hfinal=h
-    return (shortest_dist,hfinal)
-   
-def MIP_range(particle):
-    '''
-    Returns the range of a mip candidate, with uncontained particles recieving zero length
+    return (shortest_dist,[hfinal,particle])
 
-    Parameters
-    ----------
-    particle : spine.Particle
-        Particle object with cluster information
-    
-    Returns
-    -------
-    float
-        Range of particle
-    '''
-    if not particle.is_contained: return 0
-    return np.linalg.norm(particle.end_position-particle.position)
-
-def daughters(particle,particle_list):
+def daughters(particle:FinalParticle,particle_list):
     '''
     Returns number of daughter candidates with each semantic segmentation prediction.
 
@@ -177,10 +196,10 @@ def daughters(particle,particle_list):
     daughter_dist=np.inf
     for p in particle_list:
         if p.id==particle.id: continue
-        daughter_dist=np.min(daughter_dist,np.linalg.norm(p.position-particle.end_position))
+        daughter_dist=np.minimum(daughter_dist,np.linalg.norm(p.position-particle.end_point))
     return daughter_dist
     
-def MIP_to_michel(michel,kmupairs):
+def MIP_to_michel(michel:FinalParticle,kmupairs:list[list[FinalParticle]]):
     '''
     Returns minimum distance between the start of a michel candidate and the end
     of every mip candidate supplied, along with the hip/mip pair identified.
@@ -191,127 +210,39 @@ def MIP_to_michel(michel,kmupairs):
     ----------
     michel : spine.Particle
         Particle object with cluster information
-    kmupairs: List([int,spine.Particle,spine.Particle])
-        List of spine particle objects corresponding to identified k/mu pairs along with the associated event number
+    kmupairs: List([spine.Particle,spine.Particle])
+        List of spine particle objects corresponding to identified k/mu pairs along
     
     Returns
     -------
-    (float,[int, spine.Particle,spine.Particle])
-        Distance from michel to mip candidate and a list of event number,identified kaon candidate, identified muon candidate
+    (float,[spine.Particle,spine.Particle])
+        Distance from michel to mip candidate and a list of identified kaon candidate, identified muon candidate
     '''
     mindist=np.inf
     if michel.shape!=MICHL_SHP:return np.inf
+    pfinal=[]
     for p in kmupairs:
-        dist=np.linalg.norm(michel.position-p[1].end_position)
-        mindist=np.min(mindist,dist)
-    return (mindist,p)
+        dist=np.linalg.norm(michel.start_point-p[1].end_point)
+        mindist=np.minimum(mindist,dist)
+        pfinal=p
+    return (mindist,pfinal+[michel])
     
-def true_k_with_mu(particle_list):
-    '''
-    Returns track_ids for kaons which are contained and which only have a muon which is both contained and full range
 
+def potential_lambda():
+    pass
+    '''
+    Returns true if various checks (TBD) on a potential lambda pass
+    #TODO something else to check what other particles begin near the lambda start point.
+    # I only want a proton and a pion, nothing else
     Parameters
     ----------
-    particle_list: List(spine.Particle)
-        List of spine particle objects
-    
     Returns
     -------
-    List
-        Shape (n) track ids for true kaons satisfying cuts
     '''
-    K_pdgs={}
-    for p in range(particle_list):
-        if p.parent_pdg_code==321 and ((p.is_contained and abs(p.pdg)==13) or p.processid=="4::121"):
-            if p.parent_id not in K_pdgs: K_pdgs[p.parent_id]=[]
-            K_pdgs[p.parent_id].append(abs(p.pdg)*(p.processid!="4::121"))
-    for i in list(K_pdgs.keys()):
-        if set(K_pdgs[i])!=set([13]):
-            K_pdgs.pop(i)
-    return list(K_pdgs.keys())
-
-def true_lambda(particle_list):
-    '''
-    Returns track_ids for true p/pi pairs which are both contained and full range
-
-    Parameters
-    ----------
-    particle_list: List(spine.Particle)
-        List of spine particle objects
-    
-    Returns
-    -------
-    List([int,int])
-        List of contained pion/proton pairs which originate from true lambdas
-    '''
-    lambda_pdgs={}
-    for p in range(particle_list):
-        if p.parent_pdg_code==3122 and ((p.is_contained and abs(p.pdg) in [2212,211]) or p.processid=="4::121"):
-            if p.parent_id not in lambda_pdgs: lambda_pdgs[p.parent_id]=[]
-            lambda_pdgs[p.parent_id].append(abs(p.pdg)*(p.processid!="4::121"))
-    for i in list(lambda_pdgs.keys()):
-        if set(lambda_pdgs[i])!=set([2212,211]):
-            lambda_pdgs.pop(i)
-    return list(lambda_pdgs.keys())
-
-def potential_lambda_hip(hip):
-    '''
-    Returns true iff a particle object is a contained predicted hip
-
-    Parameters
-    ----------
-    hip: spine.Particle
-        spine particle object
-    
-    Returns
-    -------
-    bool
-        True iff particle is a contained predicted hip
-    '''
-    if not hip.is_contained: return False
-    if HIPMIP_pred(hip,etc)!=HIP: return False
     return True
 
-def potential_lambda_mip(mip):
-    '''
-    Returns true iff a particle object is a contained predicted mip
-
-    Parameters
-    ----------
-    mip: spine.Particle
-        spine particle object
-    
-    Returns
-    -------
-    bool
-        True iff particle is a contained predicted mip
-    '''
-    if not mip.is_contained: return False
-    if HIPMIP_pred(mip,etc)!=MIP: return False
-    return True
-
-def potential_lambda(hip,mip):
-    '''
-    Returns distance from hip candidate start to mip candidate start
-
-    Parameters
-    ----------
-    hip: spine.Particle
-        spine particle object
-    mip: spine.Particle
-        spine particle object
-    
-    Returns
-    -------
-    float
-        distance from start point of two given particles
-    '''
-    return np.linalg.norm(hip.position-mip.position)
-
-#TODO something else to check what other particles begin near the lambda start point.
-# I only want a proton and a pion, nothing else
-    
-def lambda_decay_len(hip,mip):
+def lambda_decay_len(hip:FinalParticle,mip:FinalParticle):
+    pass
     '''
     Returns distance from average start position of hip and mip to vertex location of the assocated interaction
 
@@ -327,9 +258,11 @@ def lambda_decay_len(hip,mip):
     float
         distance from lambda decay point to vertex of interaction
     '''
-    guess_start=(hip.position+mip.position)/2
-    neutrino_int=(neutrino associated to interaction/hip/mip).position
-    return np.linalg.norm(guess_start-neutrino_int)
+    # guess_start=(hip.start_point+mip.start_point)/2
+    return 0
+
+    #TODO get vertex associated to this interaction
+    # return np.linalg.norm(guess_start-hip.??????)
    
 def lambda_kinematic(hip,mip):
     '''
@@ -349,4 +282,54 @@ def lambda_kinematic(hip,mip):
         difference between reconstructed lambda mass and true lambda mass
     '''
     LAM_MASS=1115.60 #lambda mass in MeV
-    return 2*(PROT_MASS*mip.KE+hip.KE*PION_MASS-np.dot(hip.momentum,mip.momentum))+(PROT_MASS+PI_MASS)**2-LAM_MASS**2
+    return 2*(PROT_MASS*mip.KE+hip.KE*PION_MASS-np.dot(hip.momentum,mip.momentum))+(PROT_MASS+PION_MASS)**2-LAM_MASS**2
+
+
+
+# def true_k_with_mu(particle_list):
+#     '''
+#     Returns track_ids for kaons which are contained and which only have a muon which is both contained and full range
+
+#     Parameters
+#     ----------
+#     particle_list: List(spine.Particle)
+#         List of spine particle objects
+    
+#     Returns
+#     -------
+#     List
+#         Shape (n) track ids for true kaons satisfying cuts
+#     '''
+#     K_pdgs={}
+#     for p in range(particle_list):
+#         if p.parent_pdg_code==321 and ((p.is_contained and abs(p.pdg)==13) or p.processid=="4::121"):
+#             if p.parent_id not in K_pdgs: K_pdgs[p.parent_id]=[]
+#             K_pdgs[p.parent_id].append(abs(p.pdg)*(p.processid!="4::121"))
+#     for i in list(K_pdgs.keys()):
+#         if set(K_pdgs[i])!=set([13]):
+#             K_pdgs.pop(i)
+#     return list(K_pdgs.keys())
+
+# def true_lambda(particle_list):
+#     '''
+#     Returns track_ids for true p/pi pairs which are both contained and full range
+
+#     Parameters
+#     ----------
+#     particle_list: List(spine.Particle)
+#         List of spine particle objects
+    
+#     Returns
+#     -------
+#     List([int,int])
+#         List of contained pion/proton pairs which originate from true lambdas
+#     '''
+#     lambda_pdgs={}
+#     for p in range(particle_list):
+#         if p.parent_pdg_code==3122 and ((p.is_contained and abs(p.pdg) in [2212,211]) or p.processid=="4::121"):
+#             if p.parent_id not in lambda_pdgs: lambda_pdgs[p.parent_id]=[]
+#             lambda_pdgs[p.parent_id].append(abs(p.pdg)*(p.processid!="4::121"))
+#     for i in list(lambda_pdgs.keys()):
+#         if set(lambda_pdgs[i])!=set([2212,211]):
+#             lambda_pdgs.pop(i)
+#     return list(lambda_pdgs.keys())
--- run_analysis.py
+++ run_analysis.py
@@ -71,12 +71,14 @@ import sys
 
 
 
+from analysis.analysis_cuts import direction_acos
 from spine.io.read import HDF5Reader
 
 import yaml
 from spine.driver import Driver
 
-from analysis import *
+from analysis.analysis_cuts import *
+import numpy as np
 
 # filepath=sys.argv[1]
 # file=os.path.basename(filepath)
@@ -93,8 +95,8 @@ DATA_DIR = '/home/' # Change this path if you are not on SDF (see main README)
 # Set software directory
 sys.path.append(SOFTWARE_DIR)
 
-def main(min_hip_range,min_forwardness,max_hip_to_mip_dist,mip_range,
-         max_michel_dist,min_lambda_decay_len,lambda_kinematic_bounds,is_sim=False):
+def main(min_hip_range=0,max_acos=0,max_hip_to_mip_dist_K=np.inf,mip_range=[0,np.inf],
+         max_michel_dist=np.inf,min_lambda_decay_len=0,lambda_kinematic_bounds=[-np.inf,np.inf],max_hip_to_mip_dist_lam=np.inf):
 
     newsemseg="utils/output_HM.h5"
 
@@ -113,88 +115,94 @@ def main(min_hip_range,min_forwardness,max_hip_to_mip_dist,mip_range,
     ######read in the analysis file that everyone looks at##################
 
 
-    # min_hip_range=something
-    # min_forwardness=something
-    # max_hip_to_mip_dist=something
-    # mip_range=[something,something]
-    # max_michel_dist=something
+    potential_K=[]
+    predicted_K=[]
+    predicted_K_michel=[]
 
-    # min_lambda_decay_len=something
-    # lambda_kinematic_bounds=[something,something]
-
-    potential_K={}
-    predicted_K={}
-    predicted_K_michel={}
-
-    predicted_L={}
+    predicted_L=[]
 
 
 
     print("starting")
 
-    for ENTRY_NUM in range(driver.max_iteration):
+    for ENTRY_NUM in range(len(driver)):
         print(ENTRY_NUM)
         data = driver.process(entry=ENTRY_NUM)
         hipmip= reader[ENTRY_NUM]
         print(reader['seg_label'])
 
-        true_kaons=[]
-        true_lambdas=[]
+        # true_kaons=[]
+        # true_lambdas=[]
 
         # HIP=1
         # MIP=2
 
         particles=something
+        sparse3d_pcluster_semantics_HM=something2
+
+        #STANDARD [SHOWR_SHP, TRACK_SHP, MICHL_SHP, DELTA_SHP, LOWES_SHP,UNKWN_SHP]
+
+        #HM [SHOWR_SHP, HIP_SHP, MIP_SHP, MICHL_SHP, DELTA_SHP, LOWES_SHP,UNKWN_SHP]
 
         # FIND PRIMARY KAONS LOOP---------------------
         for hip_candidate in particles:
-            if not hip_candidate.is_primary: continue
-            if HIP_range(hip_candidate)<min_hip_range : continue
-            if forwardness(hip_candidate)<min_forwardness: continue
-            if daughters(hip_candidate,particles)!=[one muon]: continue
-            potential_K[ENTRY_NUM]+=[[ENTRY_NUM,hip_candidate.trackid]]
+            if not hip_candidate.is_primary: continue #PRIMARY
+            if HIPMIP_pred(hip_candidate,sparse3d_pcluster_semantics_HM)!=HIP: continue #HIP
+            if not is_contained(hip_candidate): continue #CONTAINED
+            if hip_candidate<min_hip_range: continue #RANGE
+            if direction_acos(hip_candidate)>max_acos: continue #FORWARD
+            if daughters(hip_candidate,particles)!=[0,0,1,0,0,0,0]: continue #EXTRA DAUGHTERS
+            potential_K+=[[hip_candidate]]
 
         for mip_candidate in particles:
-            if MIP_range(mip_candidate)<mip_range[0]: continue
-            if MIP_range(mip_candidate)>mip_range[1]: continue
-            z=dist_hipend_mipstart(mip_candidate,potential_K[ENTRY_NUM])
-            if z[0]>max_hip_to_mip_dist:continue
-            if daughters(mip_candidate,particles)!=[None or one michel]:continue
-            predicted_K[ENTRY_NUM]+=[z[1]+[mip_candidate.trackid]]
+            if HIPMIP_pred(mip_candidate,sparse3d_pcluster_semantics_HM)!=MIP: continue #MIP
+            if not is_contained(mip_candidate): continue #CONTAINED
+            if mip_candidate.length<mip_range[0] or mip_candidate.length>mip_range[1]:continue #RANGE
+            z=dist_hipend_mipstart(mip_candidate,potential_K) 
+            if z[0]>max_hip_to_mip_dist_K:continue #START NEAR END OF ONE OF PREV KAONS
+            daughters=daughters(mip_candidate,particles)
+            if (daughters!=[0,0,1,0,0,0,0] and np.linalg.norm(daughters)!=0):continue #EXTRA DAUGHTERS
+                
+            predicted_K+=[z[1]+[mip_candidate]]
 
         for michel_candidate in particles:
+            if HIPMIP_pred(michel_candidate,sparse3d_pcluster_semantics_HM)!=MICHL_SHP: continue #MICHL
             z=MIP_to_michel(michel_candidate,predicted_K)
-            if z[0]>max_michel_dist:continue
-            predicted_K_michel+=[z[1]+[michel_candidate.trackid]]
+            if z[0]>max_michel_dist:continue #START NEAR END OF ONE OF PREV MUONS
+            predicted_K_michel+=[z[1]+[michel_candidate]]
         # END FIND PRIMARY KAONS LOOP---------------------
 
         # FIND LAMBDAS LOOP---------------------------
         for lam_hip_candidate in particles:
-            if not potential_lambda_hip(lam_hip_candidate): continue
+            if not is_contained(lam_hip_candidate): continue #CONTAINED
+            if HIPMIP_pred(lam_hip_candidate,sparse3d_pcluster_semantics_HM)!=HIP: continue #HIP
             for lam_mip_candidate in particles:
-                if not potential_lambda_mip(lam_mip_candidate): continue
-                if lambda_decay_len(lam_hip_candidate)<min_lambda_decay_len: continue
-                if lambda_kinematic(lam_hip_candidate,lam_mip_candidate)<lambda_kinematic_bounds[0] or lambda_kinematic(lam_hip_candidate,lam_mip_candidate)>lambda_kinematic_bounds[1]: continue
+                if not is_contained(lam_mip_candidate): continue #CONTAINED
+                if HIPMIP_pred(lam_mip_candidate,sparse3d_pcluster_semantics_HM)!=MIP: continue #MIP
+
+
+                if np.linalg.norm(lam_hip_candidate.position-lam_mip_candidate.position)>max_hip_to_mip_dist_lam: continue  #DIST FROM MIP TO HIP START
+                if lambda_decay_len(lam_hip_candidate,lam_mip_candidate)<min_lambda_decay_len: continue #EFFECTIVE DECAY LENGTH
+                if lambda_kinematic(lam_hip_candidate,lam_mip_candidate)<lambda_kinematic_bounds[0] or lambda_kinematic(lam_hip_candidate,lam_mip_candidate)>lambda_kinematic_bounds[1]: continue #KINEMATIC
                 #TODO there's maybe some daughter cut I could use here?
-                predicted_L+=[[ENTRY_NUM,lam_hip_candidate.trackid,lam_mip_candidate.trackid]]
+                predicted_L+=[[lam_hip_candidate.trackid,lam_mip_candidate.trackid]]
         # END FIND LAMBDAS LOOP---------------------------
 
-        #TODO below this line gets less and less thought out
-        if is_sim:
-            pk=[tuple(j) for i in predicted_K.values() for j in i]
+        # if is_sim:
+        #     pk=[tuple(j) for i in predicted_K.values() for j in i]
             
-            efficiency_K=len(set(predicted_K)&set(true_kaons))/len(true_kaons) #total efficiency
-            purity_K=len(set(predicted_K)&set(true_kaons))/len(predicted_K) #total purity
+        #     efficiency_K=len(set(predicted_K)&set(true_kaons))/len(true_kaons) #total efficiency
+        #     purity_K=len(set(predicted_K)&set(true_kaons))/len(predicted_K) #total purity
 
-            pkm=[tuple(j) for i in predicted_K_michel.values() for j in i]
+        #     pkm=[tuple(j) for i in predicted_K_michel.values() for j in i]
 
-            efficiency_K_mich=len(set(predicted_K_michel)&set(true_kaons_michel))/len(true_kaons_michel) #total efficiency
-            purity_K_mich=len(set(predicted_K_michel)&set(true_kaons_michel))/len(predicted_K_michel) #total purity
+        #     efficiency_K_mich=len(set(predicted_K_michel)&set(true_kaons_michel))/len(true_kaons_michel) #total efficiency
+        #     purity_K_mich=len(set(predicted_K_michel)&set(true_kaons_michel))/len(predicted_K_michel) #total purity
 
-            pL=[tuple(j) for i in predicted_L.values() for j in i]
+        #     pL=[tuple(j) for i in predicted_L.values() for j in i]
 
-            efficiency_L=len(set(predicted_L)&set(true_lambdas))/len(true_lambdas) #total efficiency
-            purity_L=len(set(predicted_L)&set(true_lambdas))/len(predicted_L) #total purity
+        #     efficiency_L=len(set(predicted_L)&set(true_lambdas))/len(true_lambdas) #total efficiency
+        #     purity_L=len(set(predicted_L)&set(true_lambdas))/len(predicted_L) #total purity
 
         
 
